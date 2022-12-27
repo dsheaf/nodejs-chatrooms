6a5346d2f262896162ad80b6d736c96187a3744c
dsheaf <121428677+dsheaf@users.noreply.github.com>
2022-12-27 11:00:03 -0800

    Seperate rooms, user listing, ui improvements, room listing, room creation (on the fly as joined)

--- index.js
+++ index.js
@@ -7,17 +7,60 @@ app.use(express.static('public'));
 server.listen(8080);
 
 const users = {};
+const room = {};
+
+function listusers(where, who) {
+	const userlist = [];
+	if (io.of("/").adapter.rooms.get(where) != undefined ) {
+	io.of("/").adapter.rooms.get(where).forEach(function (item, id) {
+			userlist.push(users[item]);
+	});
+	io.in(where).emit('user-list', userlist);
+	} else {
+	return;
+	}
+}
 
 io.on('connection', socket => {
+	socket.on('join', name => {
+		if ( /^#[a-zA-Z0-9.-_]+$/.test(name) ) {
+		socket.leaveAll();
+		socket.join(socket.id); //Fixes Duplication bug, not sure the reason for including it
+		socket.join(name);
+		room[socket.id] = name;
+		socket.emit("joined", { name: name, success: true });
+		} else {
+		socket.emit("joined", { name: name, success: false });
+		}
+	})
+	socket.on('change-chat', name => {
+		registerroom("/" + name);
+		socket.emit("room-change-success", name);
+	})
+	socket.on('list-rooms', () => {
+			rooms = "";
+			io.of("/").adapter.rooms.forEach(function (item, id) {
+				if (/^#/.test(id)) {
+						rooms = rooms + id + " ";
+				}
+			});
+			socket.emit('room-list', rooms);
+	})
+	socket.on('list-users', room => {
+			listusers(room, socket.id);
+	})
 	socket.on('send-message', message => {
-		socket.broadcast.emit('message', { message: message, name: users[socket.id] });
+		socket.to(room[socket.id]).emit('message', { message: message, name: users[socket.id] });
+		console.log(io.of('/').adapter.rooms);
 	})
 	socket.on('new-user', name => {
 		users[socket.id] = name;
-		socket.broadcast.emit('user-connected', name);
+		listusers(room[socket.id], socket.id);
+		socket.to(room[socket.id]).emit('user-connected', name);
 	})
 	socket.on('disconnect', name => {
-		socket.broadcast.emit('user-disconnected', users[socket.id]);
+		socket.to(room[socket.id]).emit('user-disconnected', users[socket.id]);
+		listusers(room[socket.id], socket.id);
 		delete users[socket.id];
 	})
 	socket.on('new-channel', name => {
@@ -29,31 +72,3 @@ io.on('connection', socket => {
 		delete users[socket.id];
 	})
 })
-
-/*
-var rooms = io.of(/^.*$/);
-
-rooms.on('connection', socket => {
-		console.log("connection!")
-	const namespace = socket.nsp;
-	socket.on('send-message', message => {
-		rooms.emit('message', { message: message, name: users[socket.id] });
-	})
-	socket.on('new-user', name => {
-		users[socket.id] = name;
-		rooms.emit('user-connected', name);
-	})
-	socket.on('disconnect', name => {
-		rooms.emit('user-disconnected', users[socket.id]);
-		delete users[socket.id];
-	})
-	socket.on('new-channel', name => {
-		rooms.emit('user-disconnected', users[socket.id]);
-		delete users[socket.id];
-	})
-	socket.on('delete-channel', name => {
-		rooms.emit('user-disconnected', users[socket.id]);
-		delete users[socket.id];
-	})
-})
-*/
--- public/script.js
+++ public/script.js
@@ -1,7 +1,11 @@
 const socket = io('http://localhost:8080');
 const compose = document.getElementById('compose');
+const newchatform = document.getElementById('new-chat-form');
 const message = document.getElementById('message');
 const messages = document.getElementById('messages');
+const createchat = document.getElementById('create-chat');
+const chatname = document.getElementById('chat-name');
+const users = document.getElementById('users');
 const badchars = {
   '&': '&amp;',
   '<': '&lt;',
@@ -18,15 +22,15 @@ function sanitize(data) {
   });
 }
 
-
 var room = window.location.hash;
 //var rsocket = io("/" + room);
 var rsocket = socket;
 var name = prompt('Enter Username');
-name = name;
-appendmessage('Welcome to ' + sanitize(room));
-appendmessage('You joined as <b>' + sanitize(name) + '</b>');
-rsocket.emit('new-user', name);
+if ( room != "") {
+socket.emit('join', room);
+} else {
+socket.emit('list-rooms');
+}
 
 rsocket.on('message', data => {
 	appendmessage("<b>" + sanitize(data.name) + ":</b> " + sanitize(data.message));
@@ -37,12 +41,49 @@ rsocket.on('user-connected', name => {
 rsocket.on('user-disconnected', name => {
 	appendmessage("<b>" + sanitize(name) + "</b> disconnected");
 })
+socket.on('room-creation-success', name => {
+	window.location.hash = name;
+	socket.emit('join', name);
+})
+socket.on('room-list', rooms => {
+	appendmessage('Known Rooms: ' + rooms);
+	appendmessage('Join them using "Join Chat", or create a new one.');
+})
+socket.on('user-list', userlist => {
+	users.innerHTML = "";
+	userlist.forEach(function (user, id) {
+		const userelem = document.createElement('div');
+		if ( user == name ) {
+			userelem.innerHTML = "<b><i>" + user + "</i></b>";
+		} else {
+			userelem.innerHTML = "<b>" + user + "</b>";
+		}
+		users.append(userelem);
+	});
+})
+
+socket.on('joined', roomname => {
+	if ( roomname.success == false) {
+			alert("Failed to change room\nName must be only a-zA-Z0-9.-_");
+	} else {
+	appendmessage('Welcome to ' + sanitize(roomname.name));
+	appendmessage('You joined as <b>' + sanitize(name) + '</b>');
+	document.title = roomname.name;
+	room = roomname.name;
+	rsocket.emit('new-user', name);
+	}
+})
 
 compose.addEventListener('submit', e => {
 	e.preventDefault();
 	appendmessage('<b><i>You:</i></b> ' + sanitize(message.value))
 	rsocket.emit('send-message', message.value);
 	message.value = '';
+})
+newchatform.addEventListener('submit', e => {
+	e.preventDefault();
+	socket.emit('join', '#' + chatname.value);
+	chatname.value = '';
 })
 
 function appendmessage(message) {
